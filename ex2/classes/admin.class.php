602b78c4044cbd23fffb05fae2e7ab002d0a24b7
JavaMoose <j_drich08@yahoo.com>
2018-09-11 12:48:20 -0400

    Add files via upload

---
+++ ex2/classes/admin.class.php
@@ -0,0 +1,24 @@
+<?
+class admin extends user {
+
+  public function __construct($level, $id) {
+    parent::__construct($level);
+    $this->user_id = $id;
+    $this->user_type = '2';
+  }
+
+  public function __set($name, $value) {
+    $this->$name = $value;
+    return;
+  }
+
+  public function __get($name) {
+    return $this->$name;
+  }
+
+  public function __destruct() {
+
+  }
+
+}
+?>
